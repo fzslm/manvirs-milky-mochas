57fe2c112dc5e633623c47889ce2daf87e54cf0d
Faiz Saleem <hi@fzslm.me>
2017-02-19 18:25:34 +0000

    loading of background textures added

--- peony/lib/includes.js
+++ peony/lib/includes.js
@@ -2,3 +2,4 @@ var fs = require('fs-extra');			/* includes fs-extra, used for advanced file sys
 var jsonfile = require('jsonfile');		/* includes jsonfile, used to load JSON files as objects */
 var gui = require('nw.gui');			/* includes nw.gui, used to manipulate NW.js client */
 var win = gui.Window.get();				/* includes window object, allows us to manipulate the window */
+var sizeOf = require('image-size');		/* includes sizeOf, used to determine width + height of sprites */
