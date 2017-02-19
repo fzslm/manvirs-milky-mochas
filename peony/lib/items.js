e147afef802f164a14282347b05c8c89c307e7e4
Faiz Saleem <hi@fzslm.me>
2017-02-19 21:52:02 +0000

    cleanup Item constructor

--- peony/lib/items.js
+++ peony/lib/items.js
@@ -37,8 +37,6 @@ function Item(id) {
 	this.state = itemDefinitions[id].state;
 	this.dimensions = itemDefinitions[id].dimensions;
 	this.position = itemDefinitions[id].position;
-	this.level = 1;
-	this.image = "assets/set1/chair.png";
 	this.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 	    return v.toString(16);
