11a1b0a7b5da45a0f9c4b18da014e7ed8afb6bfc
Faiz Saleem <hi@fzslm.me>
2017-02-19 21:45:11 +0000

    we can now add items

---
+++ peony/lib/items.js
@@ -0,0 +1,36 @@
+var itemDefinitions = [
+	{
+		"name": "Chair",
+		"description": "A simple wooden chair. Patrons can sit on them while they eat and drink.",
+		"price": 100,
+		"maxLevel": 1,
+		"canNPCInteract": true,
+		"interaction": function() {
+			alert('hi!');
+		},
+		"state": "vacant",
+		"dimensions": [274,340],
+		"position": [512,640]
+	}
+];
+
+function Item(id) {
+	this.name = itemDefinitions[id].name;
+	this.description = itemDefinitions[id].description;
+	this.price = itemDefinitions[id].price;
+	this.maxLevel = itemDefinitions[id].maxLevel;
+	this.canNPCInteract = itemDefinitions[id].canNPCInteract;
+	this.interaction = itemDefinitions[id].interaction;
+	this.state = itemDefinitions[id].state;
+	this.dimensions = itemDefinitions[id].dimensions;
+	this.position = itemDefinitions[id].position;
+	this.level = 1;
+	this.image = "assets/set1/chair.png";
+	this.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+	    return v.toString(16);
+	});
+
+	$('.viewContent').append('<div class="item" style="width: '+this.dimensions[0]+'px; height: '+this.dimensions[1]+'px; top: '+this.position[1]+'px; left: '+this.position[0]+'px; position: relative; background: url(\''+this.image+'\')" id="item-'+this.uuid+'"></div>');
+
+}
