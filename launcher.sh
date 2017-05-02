6faac868e8147608b0ba2167dd33c7dc6cc29681
hi@fzslm.me <fzslm@users.noreply.github.com>
2017-05-02 21:16:47 +0100

    rewrote launcher (and removed blatant profanity)

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 echo starting...
-echo i apologise if the game\'s too shit for your tastes
-echo believe me it\'s certainly too shit for mine
-echo 
-/Applications/nwjs.app/Contents/MacOS/nwjs .
+dep/nwjs.app/Contents/MacOS/nwjs .
